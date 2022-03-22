14e75549d5f2d4bb731f89d939b53d12e523aa99
Guilherme Yuji Koyama Soken <guilhermesoken2001@gmail.com>
2022-03-22 02:07:23 -0300

    Inserção de usuário com POST

--- src/main/java/com/guilhermeyuji/workshopmongodb/services/UserService.java
+++ src/main/java/com/guilhermeyuji/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilhermeyuji.workshopmongodb.domain.User;
+import com.guilhermeyuji.workshopmongodb.dto.UserDTO;
 import com.guilhermeyuji.workshopmongodb.repository.UserRepository;
 import com.guilhermeyuji.workshopmongodb.services.exceptions.ObjectNotFoundException;
 
@@ -15,13 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-}
+	}
+
+	public User Insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fomDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
